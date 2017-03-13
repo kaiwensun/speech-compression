a0d16ad51d049ea97ed5f1bd9276e8be4826c12d
kaiwensun <skw_kevin@126.com>
2017-03-13 10:16:26 -0700

    add comment for power computation

--- code/powerComputation.m
+++ code/powerComputation.m
@@ -1,3 +1,12 @@
+%% compute power for each frame for generation of pulse train.
+% @author Kaiwen Sun
+% @param errorFrames: L-by-n frames. Each column is a frame.
+% @param periods: 1-by-n vector. The period for each frame, in the unit of
+% number of samples.
+% @param unvoicingInd: 1-by-n vector. Inidcates whether a frame is unvoiced.
+% @param voicingInd: 1-by-n vector. Indicates whether a frame is voicied.
+% @return power: 1-by-n vector. Indicates the power of each frame. The
+% power of voiced and unvoiced frames are computed in different ways.
 function [ power ] = powerComputation(errorFrames, periods,unvoicingInd,voicingInd) 
     power = zeros(1,size(errorFrames,2));
     %% power of unvoiced frames
