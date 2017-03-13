0a8dec01b4add0e7388078cfd05579393ac84337
kaiwensun <skw_kevin@126.com>
2017-03-12 19:45:46 -0700

    calculate levinson-durbin for multiple frames in matrix

--- code/levinsonDurbin.m
+++ code/levinsonDurbin.m
@@ -1,17 +1,25 @@
-function [ a,e,k,R] = levinsonDurbin( frames, order )
+%% find Levinson-Durbin parameters for frames
+% @param frames: L-by-n matrix. L is frame size. n is number of frames.
+% @param order: the order of levinson
+% @return a: n-by-(order+1) matrix. a(:,1) is always ones. Approximatly 
+% a[0]*R[0] = a[1]*R[1]+a[2]*R[2]*...*a[p]*R[p].
+% @return e: n-by-(order+1) matrix. e(:,order+1) is the final error.
+% e(:,j) is the error in the j-th recursion.
+% @return k: n-by-(order+1) matrix. PARCOR coeffieients. k(:,1) is always
+% zeros.
+% @return R: (order+1)-by-n matrix. Auto-correlation.
+function [a,e,k,R] = levinsonDurbin( frames, order )
+    sz = size(frames);
     R = getHalfAutocorrelation(frames, order);
-    R = R(:,9);
-    R = [5;4;3;2;1];
-    a = [ones(1,1),zeros(1,order)];
-    e = [R(1,1),zeros(1,order)];
-    k = zeros(1,order+1);
+    a = [ones(sz(2),1),zeros(sz(2),order)];
+    e = [R(1,:)',zeros(sz(2),order)];
+    k = zeros(sz(2),order+1);
     for i = 1:order
-        k(1,i+1) = (R(i+1,1)-a(1,2:i)*flipud(R(2:i,1)))/e(i);
-        a(1,2:i) = a(1,2:i)-k(1,i+1)*fliplr(a(1,2:i));
-        a(1,i+1) = k(1,i+1);
-        e(1,i+1) = (1-k(1,i+1).^2)*e(1,i);
+        k(:,i+1) = (R(i+1,:)'-diag(a(:,2:i)*flipud(R(2:i,:))))./e(:,i);
+        a(:,2:i) = a(:,2:i)-bsxfun(@times,k(:,i+1),fliplr(a(:,2:i)));
+        a(:,i+1) = k(:,i+1);
+        e(:,i+1) = (1-k(:,i+1).^2).*e(:,i);
     end
-    
 end
 
 function R = getHalfAutocorrelation(frames, order)
